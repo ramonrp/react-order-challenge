8c41bc362cd8ad28d212c8c28058e3d127e384d9
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-01-23 10:00:01 +0100

    refactor: split in two components and add memo

--- src/components/OrderHeader.tsx
+++ src/components/OrderHeader.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { formatDate } from '../utils';
 interface Props {
   id: number;
@@ -10,22 +11,48 @@ interface Props {
 
 const OrderHeader: React.FC<Props> = props => {
   const { id, supplier, date, totalPrice, onSendData, percentageCorrect } = props;
+
+  return (
+    <div>
+      <HeaderInfo id={id} supplier={supplier} date={date} />
+      <HeaderPrice
+        totalPrice={totalPrice}
+        onSendData={onSendData}
+        percentageCorrect={percentageCorrect}
+      />
+    </div>
+  );
+};
+
+interface InfoProps {
+  id: number;
+  supplier: string;
+  date: string;
+}
+const HeaderInfo: React.ExoticComponent<InfoProps> = React.memo(({ id, supplier, date }) => {
   const formattedDate = formatDate(date);
+  return (
+    <div>
+      <p>NºPedido:{id}</p>
+      <p>Proveedor:{supplier}</p>
+      <p>Fecha:{formattedDate}</p>
+    </div>
+  );
+});
+interface PriceProps {
+  totalPrice: string;
+  percentageCorrect: string;
+  onSendData: () => void;
+}
+const HeaderPrice: React.FC<PriceProps> = ({ totalPrice, onSendData, percentageCorrect }) => {
   const isOrderCorrect = percentageCorrect === '100.00';
   return (
     <div>
-      <div>
-        <p>NºPedido:{id}</p>
-        <p>Proveedor:{supplier}</p>
-        <p>Fecha:{formattedDate}</p>
-      </div>
-      <div>
-        <p>Importe total:{totalPrice}</p>
-        <p>Estado:{`${percentageCorrect}% correcto`}</p>
-        <button disabled={!isOrderCorrect} onClick={onSendData}>
-          Enviar
-        </button>
-      </div>
+      <p>Importe total:{totalPrice}</p>
+      <p>Estado:{`${percentageCorrect}% correcto`}</p>
+      <button disabled={!isOrderCorrect} onClick={onSendData}>
+        Enviar
+      </button>
     </div>
   );
 };
